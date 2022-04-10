9f23ee9bc520ee465aeef9565b9ae576df2ae563
Tasha <tashaasng78ws@gmail.com>
2022-04-11 00:02:26 +0800

    database fetch success

--- app/src/main/java/com/example/im2073mobile3/MainActivity.java
+++ app/src/main/java/com/example/im2073mobile3/MainActivity.java
@@ -5,18 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    TextView textView2;
-    EditText editText, editText2, editText3, editText4;
+    TextView textQuestion, textScore, textTimer;
+    Button btn1, btn2, btn3, btn4, fetchdb;
+    //EditText editText;
     private static final String DB_URL = "jdbc:mysql://192.168.1.161/MCQ";
     private static final String USER = "user1";
     private static final String PASS = "password";
@@ -26,9 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-        textView2 = (TextView) findViewById(R.id.textView2);
-        editText = (EditText) findViewById(R.id.textInputEditText);
+        textQuestion = (TextView) findViewById(R.id.textQuestion);
+        textScore = (TextView) findViewById(R.id.textView2);
+        textTimer = (TextView) findViewById(R.id.textView);
+        btn1 = (Button) findViewById(R.id.choice1);
+        btn2 = (Button) findViewById(R.id.choice2);
+        btn3 = (Button) findViewById(R.id.choice3);
+        btn4 = (Button) findViewById(R.id.choice4);
+        fetchdb = (Button) findViewById(R.id.fetchdb);
+
+        //editText = (EditText) findViewById(R.id.textInputEditText);
     }
 
     public void btnConn(View view) {
@@ -39,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
     private class Send extends AsyncTask<String, String, String>
     {
         String msg = "";
-        String text = editText.getText().toString();
+        //String text = editText.getText().toString();
 
         @Override
-        protected void onPreExecute() {textView2.setText("Please Wait...");}
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
 
         @Override
         protected String doInBackground(String... strings)
@@ -53,20 +62,30 @@ public class MainActivity extends AppCompatActivity {
                 Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
                 if(conn == null)
                 {
-                    msg = "Connection unsuccessful: conn == null";
+                    msg = "conn == null";
                 }
                 else
                 {
-                    String query = "INSERT into questionnaire (question) VALUES ('"+text+"')";
+                    //String query = "INSERT into questionnaire (question) VALUES ('"+text+"')";
+                    String query = "SELECT * from questionnaire WHERE id=10";
                     Statement stmt = conn.createStatement();
-                    stmt.executeUpdate(query);
-                    msg = "Insert successful!";
+                    ResultSet resultSet = stmt.executeQuery(query);
+
+                    while(resultSet.next())
+                    {
+                        textQuestion.setText(resultSet.getString(2));
+                        btn1.setText(resultSet.getString(3));
+                        btn2.setText(resultSet.getString(4));
+                        btn3.setText(resultSet.getString(5));
+                        btn4.setText(resultSet.getString(6));
+                    }
+                    msg = "LOADED";
                 }
                 conn.close();
             }
             catch (Exception e)
             {
-                msg = "Connection unsuccessful: insert to database error.";
+                msg = "fetch error";
                 e.printStackTrace();
             }
             return msg;
@@ -75,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String msg)
         {
-            textView2.setText(msg);
+            textScore.setText(msg);
         }
     }
 }
