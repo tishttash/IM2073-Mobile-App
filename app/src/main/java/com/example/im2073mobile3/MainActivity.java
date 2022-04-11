2c75b4196932eca734f9efd72040df85be021768
Tasha <tashaasng78ws@gmail.com>
2022-04-11 22:28:43 +0800

    load 10 qns

--- app/src/main/java/com/example/im2073mobile3/MainActivity.java
+++ app/src/main/java/com/example/im2073mobile3/MainActivity.java
@@ -54,25 +54,127 @@ public class MainActivity extends AppCompatActivity {
 //        LoadQ objSend = new LoadQ();
 //        objSend.execute("");
 //    }
+
+    // BUTTON CLICK METHODS:
     public void btnConn1(View view) {
         LoadQ1 objSend = new LoadQ1();
         objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
     }
     public void btnConn2(View view) {
         LoadQ2 objSend = new LoadQ2();
         objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
     }
     public void btnConn3(View view) {
         LoadQ3 objSend = new LoadQ3();
         objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
     }
     public void btnConn4(View view) {
         LoadQ4 objSend = new LoadQ4();
         objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
     }
     public void btnConn5(View view) {
         LoadQ5 objSend = new LoadQ5();
         objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
+    }
+    public void btnConn6(View view) {
+        LoadQ6 objSend = new LoadQ6();
+        objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
+    }
+    public void btnConn7(View view) {
+        LoadQ7 objSend = new LoadQ7();
+        objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
+    }
+    public void btnConn8(View view) {
+        LoadQ8 objSend = new LoadQ8();
+        objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
+    }
+    public void btnConn9(View view) {
+        LoadQ9 objSend = new LoadQ9();
+        objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
+    }
+    public void btnConn10(View view) {
+        LoadQ10 objSend = new LoadQ10();
+        objSend.execute("");
+        btn1 = (Button) findViewById(R.id.select1);
+        btn2 = (Button) findViewById(R.id.select2);
+        btn3 = (Button) findViewById(R.id.select3);
+        btn4 = (Button) findViewById(R.id.select4);
+        btn1.setTextColor(Color.WHITE);
+        btn2.setTextColor(Color.WHITE);
+        btn3.setTextColor(Color.WHITE);
+        btn4.setTextColor(Color.WHITE);
     }
 
     public void btnChoice(View view) {
@@ -119,6 +221,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // LOAD CONTENT INTO SCREEN CLASSES AND METHODS:
     public class LoadQ1 extends AsyncTask<String, String, String>
     {
         String msg = "";
@@ -399,7 +502,288 @@ public class MainActivity extends AppCompatActivity {
             textScore.setText(msg);
         }
     }
+    public class LoadQ6 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+        //String text = editText.getText().toString();
 
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+                    //String query = "SELECT * from questionnaire WHERE id=10";
+                    String query = "SELECT * FROM questionnaire";
+                    //TODO: add sql statement to reset counter table values to 0 0 0 0
+                    Statement stmt = conn.createStatement();
+                    ResultSet resultSet = stmt.executeQuery(query);
+
+                    resultSet.absolute(6);
+                    textQuestion.setText(resultSet.getString(2));
+                    btn1.setText(resultSet.getString(3));
+                    btn2.setText(resultSet.getString(4));
+                    btn3.setText(resultSet.getString(5));
+                    btn4.setText(resultSet.getString(6));
+                    answer = resultSet.getString(7);
+//                    while(resultSet.next())
+//                    {
+//
+//                    }
+                    msg = "LOADED | " + "Ans: " + answer;
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "fetch error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+    public class LoadQ7 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+        //String text = editText.getText().toString();
+
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+                    //String query = "SELECT * from questionnaire WHERE id=10";
+                    String query = "SELECT * FROM questionnaire";
+                    //TODO: add sql statement to reset counter table values to 0 0 0 0
+                    Statement stmt = conn.createStatement();
+                    ResultSet resultSet = stmt.executeQuery(query);
+
+                    resultSet.absolute(7);
+                    textQuestion.setText(resultSet.getString(2));
+                    btn1.setText(resultSet.getString(3));
+                    btn2.setText(resultSet.getString(4));
+                    btn3.setText(resultSet.getString(5));
+                    btn4.setText(resultSet.getString(6));
+                    answer = resultSet.getString(7);
+//                    while(resultSet.next())
+//                    {
+//
+//                    }
+                    msg = "LOADED | " + "Ans: " + answer;
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "fetch error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+    public class LoadQ8 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+        //String text = editText.getText().toString();
+
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+                    //String query = "SELECT * from questionnaire WHERE id=10";
+                    String query = "SELECT * FROM questionnaire";
+                    //TODO: add sql statement to reset counter table values to 0 0 0 0
+                    Statement stmt = conn.createStatement();
+                    ResultSet resultSet = stmt.executeQuery(query);
+
+                    resultSet.absolute(8);
+                    textQuestion.setText(resultSet.getString(2));
+                    btn1.setText(resultSet.getString(3));
+                    btn2.setText(resultSet.getString(4));
+                    btn3.setText(resultSet.getString(5));
+                    btn4.setText(resultSet.getString(6));
+                    answer = resultSet.getString(7);
+//                    while(resultSet.next())
+//                    {
+//
+//                    }
+                    msg = "LOADED | " + "Ans: " + answer;
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "fetch error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+    public class LoadQ9 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+        //String text = editText.getText().toString();
+
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+                    //String query = "SELECT * from questionnaire WHERE id=10";
+                    String query = "SELECT * FROM questionnaire";
+                    //TODO: add sql statement to reset counter table values to 0 0 0 0
+                    Statement stmt = conn.createStatement();
+                    ResultSet resultSet = stmt.executeQuery(query);
+
+                    resultSet.absolute(9);
+                    textQuestion.setText(resultSet.getString(2));
+                    btn1.setText(resultSet.getString(3));
+                    btn2.setText(resultSet.getString(4));
+                    btn3.setText(resultSet.getString(5));
+                    btn4.setText(resultSet.getString(6));
+                    answer = resultSet.getString(7);
+//                    while(resultSet.next())
+//                    {
+//
+//                    }
+                    msg = "LOADED | " + "Ans: " + answer;
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "fetch error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+    public class LoadQ10 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+        //String text = editText.getText().toString();
+
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+                    //String query = "SELECT * from questionnaire WHERE id=10";
+                    String query = "SELECT * FROM questionnaire";
+                    //TODO: add sql statement to reset counter table values to 0 0 0 0
+                    Statement stmt = conn.createStatement();
+                    ResultSet resultSet = stmt.executeQuery(query);
+
+                    resultSet.absolute(10);
+                    textQuestion.setText(resultSet.getString(2));
+                    btn1.setText(resultSet.getString(3));
+                    btn2.setText(resultSet.getString(4));
+                    btn3.setText(resultSet.getString(5));
+                    btn4.setText(resultSet.getString(6));
+                    answer = resultSet.getString(7);
+//                    while(resultSet.next())
+//                    {
+//
+//                    }
+                    msg = "LOADED | " + "Ans: " + answer;
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "fetch error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+
+    // TALLY CLASSES AND METHODS:
     public class Tally1 extends AsyncTask<String, String, String>
     {
         String msg = "";
