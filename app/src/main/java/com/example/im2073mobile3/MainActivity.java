037cc0537344f68f9fef5d99b66bdb3d99167f70
Tasha <tashaasng78ws@gmail.com>
2022-04-11 14:11:07 +0800

    tally hardcode cos I GIVE UP xd

--- app/src/main/java/com/example/im2073mobile3/MainActivity.java
+++ app/src/main/java/com/example/im2073mobile3/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -49,19 +50,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnChoice(View view) {
-        Tally objTally = new Tally();
+        Tally1 objTally1 = new Tally1();
+        Tally2 objTally2 = new Tally2();
+        Tally3 objTally3 = new Tally3();
+        Tally4 objTally4 = new Tally4();
 
-        if((((Button)view).getText()).equals(answer)) {
-            ((Button)view).setTextColor(Color.GREEN);
-            //textScore.setText(((Button)view).getText() + answer);
-            choice = (((Button)view).getTag()).toString();
-            objTally.execute("");
+        if (((((Button)view).getTag()).toString()).equals("select1")) {
+            objTally1.execute("");
+            if((((Button)view).getText()).equals(answer)) {
+                ((Button)view).setTextColor(Color.GREEN);
+            }
+            else {
+                ((Button)view).setTextColor(Color.RED);
+            }
         }
-        else {
-            ((Button)view).setTextColor(Color.RED);
-            //textScore.setText(((Button)view).getText() + answer);
-            choice = (((Button)view).getTag()).toString();
-            objTally.execute("");
+        else if (((((Button)view).getTag()).toString()).equals("select2")) {
+            objTally2.execute("");
+            if((((Button)view).getText()).equals(answer)) {
+                ((Button)view).setTextColor(Color.GREEN);
+            }
+            else {
+                ((Button)view).setTextColor(Color.RED);
+            }
+        }
+        else if (((((Button)view).getTag()).toString()).equals("select3")) {
+            objTally3.execute("");
+            if((((Button)view).getText()).equals(answer)) {
+                ((Button)view).setTextColor(Color.GREEN);
+            }
+            else {
+                ((Button)view).setTextColor(Color.RED);
+            }
+        }
+        else if (((((Button)view).getTag()).toString()).equals("select4")) {
+            objTally4.execute("");
+            if((((Button)view).getText()).equals(answer)) {
+                ((Button)view).setTextColor(Color.GREEN);
+            }
+            else {
+                ((Button)view).setTextColor(Color.RED);
+            }
         }
     }
 
@@ -120,7 +148,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class Tally extends AsyncTask<String, String, String>
+    public class Tally1 extends AsyncTask<String, String, String>
     {
         String msg = "";
 
@@ -140,10 +168,146 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    String col = "qty" + choice;
-                    //eg: UPDATE counter SET qtyselect1 = qtyselect1 + 1
-                    String query = "UPDATE counter SET" + col + " = " + col + " + 1";
-                    //String query = "UPDATE counter SET qtyselect1 = qtyselect1 + 1";
+//                    query i want to perform: UPDATE counter SET qtyselect(col) = qtyselect(col) + 1 --> NOTE: cannot concatenate strings!!!
+//                    PreparedStatement stmt = conn.prepareStatement("UPDATE counter SET ? = ? + 1");
+//                    stmt.setString(1, col);
+//                    stmt.setString(2, col);
+                    String query = "UPDATE counter SET qtyselect1 = qtyselect1 + 1";
+                    Statement stmt = conn.createStatement();
+                    stmt.executeUpdate(query);
+                    msg = "tally updated!";
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "tally error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+    public class Tally2 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+//                    query i want to perform: UPDATE counter SET qtyselect(col) = qtyselect(col) + 1 --> NOTE: cannot concatenate strings!!!
+//                    PreparedStatement stmt = conn.prepareStatement("UPDATE counter SET ? = ? + 1");
+//                    stmt.setString(1, col);
+//                    stmt.setString(2, col);
+                    String query = "UPDATE counter SET qtyselect2 = qtyselect2 + 1";
+                    Statement stmt = conn.createStatement();
+                    stmt.executeUpdate(query);
+                    msg = "tally updated!";
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "tally error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+    public class Tally3 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+//                    query i want to perform: UPDATE counter SET qtyselect(col) = qtyselect(col) + 1 --> NOTE: cannot concatenate strings!!!
+//                    PreparedStatement stmt = conn.prepareStatement("UPDATE counter SET ? = ? + 1");
+//                    stmt.setString(1, col);
+//                    stmt.setString(2, col);
+                    String query = "UPDATE counter SET qtyselect3 = qtyselect3 + 1";
+                    Statement stmt = conn.createStatement();
+                    stmt.executeUpdate(query);
+                    msg = "tally updated!";
+                }
+                conn.close();
+            }
+            catch (Exception e)
+            {
+                msg = "tally error";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg)
+        {
+            textScore.setText(msg);
+        }
+    }
+    public class Tally4 extends AsyncTask<String, String, String>
+    {
+        String msg = "";
+
+        @Override
+        protected void onPreExecute() {textScore.setText("Please Wait...");}
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if(conn == null)
+                {
+                    msg = "conn == null";
+                }
+                else
+                {
+//                    query i want to perform: UPDATE counter SET qtyselect(col) = qtyselect(col) + 1 --> NOTE: cannot concatenate strings!!!
+//                    PreparedStatement stmt = conn.prepareStatement("UPDATE counter SET ? = ? + 1");
+//                    stmt.setString(1, col);
+//                    stmt.setString(2, col);
+                    String query = "UPDATE counter SET qtyselect4 = qtyselect4 + 1";
                     Statement stmt = conn.createStatement();
                     stmt.executeUpdate(query);
                     msg = "tally updated!";
